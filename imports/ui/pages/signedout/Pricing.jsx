c365a7fb80e8a01d8cc65327e5ce984c5f737876
Maxime Poulet <maxime.poulet@beorntech.com>
2022-10-06 09:46:43 +0200

    Set pricing

--- imports/ui/pages/signedout/Pricing.jsx
+++ imports/ui/pages/signedout/Pricing.jsx
@@ -10,40 +10,40 @@ const Pricing = props => {
             <div className="pricing-section section-wrapper">
                 <div class="section">
                     <div class="pricing-table">
-                        <div class="pricing-plan">
+                        <div class="pricing-plan is-info">
                             <div class="plan-header">Starter</div>
                             <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>20</span>/month</div>
                             <div class="plan-items">
-                            <div class="plan-item">20GB Storage</div>
-                            <div class="plan-item">100 Domains</div>
-                            <div class="plan-item">-</div>
-                            <div class="plan-item">-</div>
-                            </div>
-                            <div class="plan-footer">
-                            <button class="button is-fullwidth" disabled="disabled">Current plan</button>
-                            </div>
-                        </div>
-                        <div class="pricing-plan is-primary">
-                            <div class="plan-header">Startups</div>
-                            <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>40</span>/month</div>
-                            <div class="plan-items">
-                            <div class="plan-item">20GB Storage</div>
-                            <div class="plan-item">25 Domains</div>
-                            <div class="plan-item">1TB Bandwidth</div>
-                            <div class="plan-item">-</div>
+                                <div class="plan-item">10 Stuctures</div>
+                                <div class="plan-item">25 Pages</div>
+                                <div class="plan-item">-</div>
+                                <div class="plan-item">-</div>
                             </div>
                             <div class="plan-footer">
                             <button class="button is-fullwidth">Choose</button>
                             </div>
                         </div>
-                        <div class="pricing-plan is-secondary">
-                            <div class="plan-header">Growing Team</div>
-                            <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>60</span>/month</div>
+                        <div class="pricing-plan is-dark">
+                            <div class="plan-header">Serious</div>
+                            <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>40</span>/month</div>
                             <div class="plan-items">
-                            <div class="plan-item">200GB Storage</div>
-                            <div class="plan-item">50 Domains</div>
-                            <div class="plan-item">1TB Bandwidth</div>
-                            <div class="plan-item">100 Email Boxes</div>
+                                <div class="plan-item">20 Stuctures</div>
+                                <div class="plan-item">100 Pages</div>
+                                <div class="plan-item">5 GB Storage</div>
+                                <div class="plan-item">-</div>
+                            </div>
+                            <div class="plan-footer">
+                            <button class="button is-fullwidth">Choose</button>
+                            </div>
+                        </div>
+                        <div class="pricing-plan is-primary">
+                            <div class="plan-header">Mastodon</div>
+                            <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>80</span>/month</div>
+                            <div class="plan-items">
+                                <div class="plan-item">100 Stuctures</div>
+                                <div class="plan-item">Unlimited Pages</div>
+                                <div class="plan-item">100 GB Storage</div>
+                                <div class="plan-item"></div>
                             </div>
                             <div class="plan-footer">
                             <button class="button is-fullwidth">Choose</button>
